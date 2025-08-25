04ce756e8e7493121997988c44dbb6e4b97db43a
josiane <josianelourenco2000@gmail.com>
2025-08-25 19:30:51 -0300

    dados atualizados

--- portas.py
+++
@@ -1,6 +0,0 @@
-from app import app
-import os
-
-if __name__ == '__main__': # executa esse código abaixo quando estiver executando essa rota
-    port = int(os.getenv('PORT', '5000'))
-    app.run(host='0.0.0.0',port=port)
